a877047350028ea57288367608bad3f3c7b0911e
cs686 <cs686@126.com>
2016-01-14 23:13:05 +0800

    Initial Commit

---
+++ TimeCount/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeCount
+//
+//  Created by cq on 16/1/14.
+//  Copyright © 2016年 顺苹亓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimeCount/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimeCount
+//
+//  Created by cq on 16/1/14.
+//  Copyright © 2016年 顺苹亓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
